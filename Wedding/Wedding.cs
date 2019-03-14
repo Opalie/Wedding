58e125deb3a06a879327a06a47b375a53fa1ba47
Allie van Crezel <allie.crezel@gmail.com>
2019-03-14 23:19:29 +0000

    changed warp method

--- Wedding/Wedding.cs
+++ Wedding/Wedding.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
@@ -23,43 +24,29 @@ namespace WeddingMod
         /// <summary>The player's tile position in the wedding map.</summary>
         private readonly Vector2 PlayerPixels = new Vector2(1732, 4052);
 
+
         /// <summary>The question before the wedding.</summary>
 
         public void setup(object sender, DayStartedEventArgs e)
         {
             if (Game1.weddingToday)
             {
-                Game1.player.currentLocation.createQuestionDialogue(
-                            this.Helper.Translation.Get("Wedding_Question"),
-                new[]
+                //Lets clean this up some
+                string question = Helper.Translation.Get("Wedding_Question");
+                List<Response> options = new List<Response>()
                 {
                     new Response("On the Beach.",this.Helper.Translation.Get("Wedding_Beach")),
                     new Response("In the Forest, near the river.",this.Helper.Translation.Get("Wedding_Forest")),
                     new Response("In the Deep Woods.",this.Helper.Translation.Get("Wedding_Wood")),
                     new Response("Where the Flower Dance takes place!",this.Helper.Translation.Get("Wedding_Flower"))
-                }, (farmer, answerKey) =>
-                {
-                    switch (answerKey)
-                    {
-                        case "On the Beach.":
-                            MapPath = "assets/WeddingBeach.tbin";
-                            // Load Beach
-                            break;
-                        case "In the Forest, near the river.":
-                            MapPath = "assets/WeddingForest.tbin";
-                            // Load Forest
-                            break;
-                        case "In the Deep Woods.":
-                            MapPath = "assets/WeddingWoods.tbin";
-                            // Load Wood
-                            break;
-                        case "Where the Flower Dance takes place!":
-                            MapPath = "assets/WeddingFlower.tbin";
-
-                            // Load Flower Dance
-                            break;
-                    }
-                }
+                };
+                /*
+                   I changed up the createQuestionDialogue, to make it easier to see. Basically, it pulls the question from the question string, and the responses from the options list, then turns it into an array. The answer part is a method that is called when the player clicks on a response.
+                */
+                Game1.player.currentLocation.createQuestionDialogue(
+                    question,
+                    options.ToArray(),
+                    answer
                 );
             }
         }
@@ -72,7 +59,7 @@ namespace WeddingMod
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Player.Warped += Player_Warped;
+            //helper.Events.Player.Warped += Player_Warped;
             helper.Events.GameLoop.DayStarted += setup;
 
         }
@@ -101,6 +88,60 @@ namespace WeddingMod
                 // move player position within map (if needed)
                 Game1.player.Position = this.PlayerPixels;
             }
+        }
+
+        /// <summary>The method that is called inside of the createQuestionDialogue</summary>
+        /// <param name="who">The player.</param>
+        /// <param name="ans">The answer key.</param>
+        private void answer(Farmer who, string ans)
+        {
+            switch (ans)
+            {
+                case "On the Beach.":
+                    MapPath = "assets/WeddingBeach.tbin";
+                    SetUpMaps();
+                    // Load Beach
+                    break;
+                case "In the Forest, near the river.":
+                    MapPath = "assets/WeddingForest.tbin";
+                    SetUpMaps();
+                    // Load Forest
+                    break;
+                case "In the Deep Woods.":
+                    MapPath = "assets/WeddingWoods.tbin";
+                    SetUpMaps();
+                    // Load Wood
+                    break;
+                case "Where the Flower Dance takes place!":
+                    MapPath = "assets/WeddingFlower.tbin";
+                    SetUpMaps();
+                    // Load Flower Dance
+                    break;
+            }
+            Game1.showGlobalMessage(MapPath);
+        }
+
+        ///<summary></summary>
+        /// 
+        private void SetUpMaps()
+        {
+            this.Monitor.Log("The wedding event has started!");
+
+            // create temporary location
+            this.Helper.Content.Load<Map>(this.MapPath); // make sure map is loaded before game accesses it
+            string mapKey = this.Helper.Content.GetActualAssetKey(this.MapPath);
+            var newLocation = new GameLocation(mapKey, "Temp");
+
+            // move everything to new location
+            var oldLocation = Game1.currentLocation;
+            Game1.player.currentLocation = Game1.currentLocation = newLocation;
+            newLocation.resetForPlayerEntry();
+            newLocation.currentEvent = oldLocation.currentEvent;
+            newLocation.Map.LoadTileSheets(Game1.mapDisplayDevice);
+
+            // move player position within map (if needed)
+            Game1.player.Position = this.PlayerPixels;
+
         }
     }
 }
